17477f22653d23a02e3ac021bdaf7ec9a641c61b
DatDraggy <spamme@kieran.de>
2018-12-13 12:21:04 +0100

    Login, Email
    Completed Login Beta
    Email Conf Request
    funcs

--- files/config.sample.php
+++ files/config.sample.php
@@ -4,3 +4,4 @@ $config['dbpassword'] = 'p4ssw0rd';
 $config['dbserver'] = 'database.example.com';
 $config['dbport'] = '3309';
 $config['mail'] = 'mail@mail.com';
+$config['sitedomain'] = 'example.com';
