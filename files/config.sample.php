99fa9813cae8f439883981a2b40e9812e6891434
DatDraggy <spamme@kieran.de>
2018-12-13 13:23:31 +0100

    Admin notif

--- files/config.sample.php
+++ files/config.sample.php
@@ -5,3 +5,6 @@ $config['dbserver'] = 'database.example.com';
 $config['dbport'] = '3309';
 $config['mail'] = 'mail@mail.com';
 $config['sitedomain'] = 'example.com';
+$config['telegramAdmins'] = array('user1' => 123, 'user2' => 1234); //Telegram userID
+$config['token'] = '123456:aefAOIEFjoauenfseljnFOUnoEAF';
+$config['url'] = 'https://api.telegram.org/bot' . $config['token'] . '/';
