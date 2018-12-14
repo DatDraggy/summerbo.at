54ab1aa31ebc1d72222c708c549dbab259e0f071
DatDraggy <spamme@kieran.de>
2018-12-14 13:25:08 +0100

    logix

--- files/config.sample.php
+++ files/config.sample.php
@@ -3,6 +3,7 @@ $config['dbuser'] = 'user';
 $config['dbpassword'] = 'p4ssw0rd';
 $config['dbserver'] = 'database.example.com';
 $config['dbport'] = '3309';
+$config['dbname'] = 'name';
 $config['mail'] = 'mail@mail.com';
 $config['sitedomain'] = 'example.com';
 $config['telegramAdmins'] = array('user1' => 123, 'user2' => 1234); //Telegram userID
