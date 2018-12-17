9ab29bcdb5ad69d7942cad56c692717087a7a0c2
haushaushaus <hausken@protonmail.ch>
2018-12-17 19:14:14 +0100

    removed vue-router, hosted own copy of vue

--- hauskenTemplates/js/app.js
+++ hauskenTemplates/js/app.js
@@ -1,30 +1,5 @@
-
-const About = { template: '<div>About</div>' }
-const Registration = { template: '<div>Registration</div>' }
-
-
-const routes = [
-  { path: '/about', component: About },
-  { path: '/registration', component: Registration }
-]
-
-const router = new VueRouter({
-  routes: [
-    {
-      path: '/about',
-      name: 'About',
-      component: About
-    },
-    {
-      path: '/registration',
-      name: 'Registration',
-      component: Registration
-    }
-  ]
-})
-
 const app = new Vue({
-  router
+
 }).$mount('#app')
 
 
