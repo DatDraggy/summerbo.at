732f90661cf88a570d46ac0e3f4784b566229e4e
haushaushaus <hausken@protonmail.ch>
2018-12-18 12:11:16 +0100

    minor fixes and added a lazy-load script

--- hauskenTemplates/js/app.js
+++ hauskenTemplates/js/app.js
@@ -21,23 +21,3 @@ theForm.addEventListener("blur", function( event ) {
 
 new BadgerAccordion('.js-badger-accordion', {
 });
-// const faqQuestions = document.querySelectorAll('.faqQuestion')
-// const faqAnswerContent = document.querySelectorAll('.faqAnswerContent')
-//
-//
-// function addClass(el, klass) {
-//   el.classList.add(klass);
-// }
-//
-// function removeClass(el, klass) {
-//   el.classList.remove(klass);
-// }
-//
-// faqQuestions.forEach(function(faq){
-//   faq.classList.add('hidden')
-//   var faqQuestion = faq.children[0]
-//   faqQuestion.addEventListener('click', showFaq)
-//   function showFaq(event) {
-//     event.target.parentElement
-//   }
-// })
