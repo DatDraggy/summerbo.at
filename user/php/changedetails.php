14280f5c10b3a69582bc4ba461ed5713824fdbde
DatDraggy <github.com@kieran.de>
2018-12-27 16:56:28 +0100

    finish update userdetails

--- user/php/changedetails.php
+++ user/php/changedetails.php
@@ -1,6 +1,6 @@
 <?php
-require_once ('../../backend/config.php');
-if(empty($_POST['userId']) || empty($_POST['nickname']) || empty($_POST['email']) || empty($_POST['passwordOld'])){
+require_once('../../backend/config.php');
+if (empty($_POST['userId']) || empty($_POST['nickname']) || empty($_POST['email']) || empty($_POST['passwordOld'])) {
   die('Details can\'t be empty');
 }
 
@@ -9,23 +9,31 @@ $nickname = $_POST['nickname'];
 $newEmail = $_POST['email'];
 $dbConnection = buildDatabaseConnection($config);
 
+/////////////////////
+// Password Verify //
 $hash = checkPassword($userId, $_POST['passwordOld']);
 
-if($hash === false){
+if ($hash === false) {
   die('Incorrect Password');
 }
+// Password Verify //
+/////////////////////
 
-if(!empty($_POST['password'])){
+/////////////////////
+// Password Change //
+if (!empty($_POST['password'])) {
   $passwordNew = $_POST['password'];
-  if(empty($_POST['passwordVerify']) || $passwordNew !== $_POST['passwordVerify']){
+  if (empty($_POST['passwordVerify']) || $passwordNew !== $_POST['passwordVerify']) {
     die('Passwords do not match');
   }
   $valid = validatePassword($passwordNew);
-  if($valid !== true){
+  if ($valid !== true) {
     die($valid);
   }
   $hash = hashPassword($passwordNew);
 }
+// Sponsor Upgrade //
+/////////////////////
 
 if (!empty($_POST['fursuiter'])) {
   $fursuiter = true;
@@ -33,24 +41,45 @@ if (!empty($_POST['fursuiter'])) {
 else {
   $fursuiter = false;
 }
+
+/////////////////////
+// Sponsor Upgrade //
 if (!empty($_POST['sponsor'])) {
-  $sponsor = true;
+  $sponsorNew = true;
 }
 else {
-  $sponsor = false;
+  $sponsorNew = false;
 }
+try {
+  $sql = "SELECT sponsor FROM users WHERE id = $userId";
+  $stmt = $dbConnection->prepare('SELECT sponsor FROM users WHERE id = :userId');
+  $stmt->bindParam(':userId', $userId);
+  $stmt->execute();
+  $row = $stmt->fetch();
+} catch (PDOException $e) {
+  notifyOnException('Database Select', $config, $sql, $e);
+}
+$sponsorOld = $row['sponsor'];
+if ($sponsorNew === true && $sponsorOld == 0) {
+  upgradeToSponsor($userId);
+  sendStaffNotification($userId, "Attendee $userId upgraded to sponsor.");
+}
+// Sponsor Upgrade //
+/////////////////////
 
-try{
+//////////////////
+// Email Change //
+try {
   $sql = "SELECT email FROM users WHERE id = $userId";
   $stmt = $dbConnection->prepare('SELECT email FROM users WHERE id = :userId');
   $stmt->bindParam(':userId', $userId);
   $stmt->execute();
   $row = $stmt->fetch();
-}catch (PDOException $e){
+} catch (PDOException $e) {
   notifyOnException('Database Select', $config, $sql, $e);
 }
 $oldEmail = $row['email'];
-if($oldEmail !== $newEmail) {
+if ($oldEmail !== $newEmail) {
   try {
     $sql = "UPDATE users SET email_new = $newEmail WHERE id = $userId";
     $stmt = $dbConnection->prepare('UPDATE users SET email_new = $email WHERE id = :userId');
@@ -72,5 +101,20 @@ Your Boat Party Crew
 ");
   }
 }
+// Email Change //
+//////////////////
 
-//ToDo: Update all Info
+/////////////////////////////////
+// Update Nickname and Fursuit //
+try {
+  $sql = "UPDATE users SET nickname = $nickname, fursuiter = $fursuiter WHERE id = $userId";
+  $stmt = $dbConnection->prepare('UPDATE users SET nickname = :nickname, fursuiter = :fursuiter WHERE id = :userId');
+  $stmt->bindParam(':nickname', $nickname);
+  $stmt->bindParam(':fursuiter', $fursuiter);
+  $stmt->bindParam(':userId', $userId);
+  $stmt->execute();
+} catch (PDOException $e) {
+  notifyOnException('Database Select', $config, $sql, $e);
+}
+// Update Nickname and Fursuit //
+/////////////////////////////////
