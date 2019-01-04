54772631e62200fc9b765b44c4aa4b54e4d3d371
Oekeokiki <45734577+Oekeokiki@users.noreply.github.com>
2019-01-04 14:25:21 +0100

    Update mailtest.php

--- backend/mailtest.php
+++ backend/mailtest.php
@@ -12,19 +12,19 @@ $nickname = 'Test';
 
 sendEmail($email, 'Summerbo.at Payment Reminder', "<p>Dear $nickname,</p>
 
-<p>Do you still want to join our party? Sadly we haven't received the payment for your ticket yet. Please make sure to pay within the coming 7 days to secure your spot on the deck. The total amount of €$remaining.- is still open. 
-Below you will find our bank details to transfer the money. </p>
+Do you still want to join our party? Sadly we haven't received the payment for your ticket yet. Please make sure to pay within the coming 7 days to secure your spot on the deck. The total amount of €$remaining.- is still open. 
+Below you will find our bank details to transfer the money.
 
-<p>Bank details:<br>
-Name: Edwin Verstaij<br>
-Bank: Bunq<br>
-IBAN: NL04 BUNQ 2290 9065 14<br>
-IC/SWIFT: BUNQNL2AXXX<br>
+<p>Bank details:
+Name: Edwin Verstaij
+Bank: Bunq
+IBAN: NL04 BUNQ 2290 9065 14
+IC/SWIFT: BUNQNL2AXXX
 Comment: $userId, $nickname</p>
 
-<p>Did you already pay everything? Please ignore this email or send us a message.</p>
+Did you already pay everything? Please ignore this email or send us a message.
 
-<p>If you have any questions, please send us a message. Reply to this e-mail or contact us via Telegram at https://t.me/summerboat.</p>
+If you have any questions, please send us a message. Reply to this e-mail or contact us via Telegram at https://t.me/summerboat.
 
-<p>Your Boat Party Crew</p>
+Your Boat Party Crew
 ", true);
