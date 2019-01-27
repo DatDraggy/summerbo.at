cd7da302422a84d6dd8f992467882fb504374373
DatDraggy <github.com@kieran.de>
2019-01-27 20:42:55 +0100

    fix

--- backend/sendMails.php
+++ backend/sendMails.php
@@ -1,4 +1,7 @@
 <?php
+if (php_sapi_name() != "cli") {
+  die('lol nah');
+}
 require_once('config.php');
 require_once('funcs.php');
 
@@ -14,5 +17,25 @@ try {
 }
 
 foreach ($rows as $row) {
-  sendEmail($row['email'], 'About your registration', '');
+  sendEmail($row['email'], 'About your registration', "Dear {$row['nickname']},
+ 
+Your registration was approved by our registration team.
+However, due to a technical difficulty, some people may have received a mail stating they got an Early Bird ticket instead of a Regular one. We would like to apologize for the inconvenience.
+ 
+Your Registration = Regular Ticket (excl. VIP.)(VIP is Status only viewable on our website)
+ 
+Your ticket price is 35 € EUR.
+Payable via <a href=\"https://reg.eurofurence.org\">Eurofurence Registration</a> starting the 25th of March.
+ 
+Are you a VIP or will you upgrade in the future?
+The 15 EUR surcharge will be paid at Badge Pickup.
+ 
+Please make sure to provide your correct EF Regnumber BEFORE the 25th of March via <a href=\"https://summerbo.at/user/details\">summerbo.at</a>
+ 
+You will then also receive an email from Eurofurence regarding the payment of your ticket.
+ 
+If you have any questions, please send us a message. Reply to this e-mail or contact us via Telegram at <a href=\"https://t.me/summerboat\">https://t.me/summerboat</a>.
+ 
+Your Boat Party Crew
+");
 }
