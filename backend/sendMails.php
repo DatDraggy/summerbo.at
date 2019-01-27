aea8f79990c6ff011e35a6110b31d19a86e975e0
DatDraggy <github.com@kieran.de>
2019-01-27 21:13:17 +0100

    better

--- backend/sendMails.php
+++ backend/sendMails.php
@@ -17,24 +17,24 @@ try {
 }
 
 foreach ($rows as $row) {
+  $nickname = $row['nickname'];
+  $efregid = $row['efregid'];
   sendEmail($row['email'], 'About your registration', "Dear $nickname,
  
 Your registration was approved by our registration team.
-However, due to a technical difficulty, some people may have received a mail stating they got an Early Bird ticket instead of a Regular one. We would like to apologize for the inconvenience.
-
-Your Registration = Regular Ticket, Ticket price 35 € EUR, excl. VIP.
-Payable via <a href=\"https://reg.eurofurence.org\">Eurofurence Registration</a> starting the 25th of March. (Yes, we will send a reminder, no worries.)
+  However, due to a technical difficulty, some people may have received a mail stating they got an Early Bird ticket instead of a Regular one. We would like to apologize for the inconvenience.
+ 
+<b>Your Registration = Regular Ticket, Ticket price 35 € EUR, excl. VIP.</b>
+  Payable via <a href=\"https://reg.eurofurence.org\">Eurofurence Registration</a> starting the 25th of March. (Yes, we will send a reminder, no worries.)
  
 Are you a VIP or will you upgrade in the future? That'd be great! Here is how we handle the extra payment:
 
 To make 100% of the VIP profits available to Eurofurence Charity we want to avoid all transaction fees and expenses. At badge pickup, a Charity staff member will directly collect your 15 Euros (you can bring more of course) and you'll get upgraded on the spot! Sounds good? We think so! 
  
-Your EF Regnumber is {$row['efregid']} - please double check that this is correct BEFORE the 25th of March via <a href=\"https://summerbo.at/user/details\">summerbo.at</a>
- 
-You will then also receive an email from Eurofurence regarding the payment of your ticket.
- 
+Your EF Regnumber is $efregid - please double check that this is correct BEFORE the 25th of March via <a href=\"https://summerbo.at/user/details\">summerbo.at</a>
+  
 If you have any questions, please send us a message. Reply to this e-mail or contact us via Telegram at <a href=\"https://t.me/summerboat\">https://t.me/summerboat</a>.
  
 Your Boat Party Crew
-");
+", true);
 }
