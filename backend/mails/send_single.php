5d0754339a7709d746ef6655a0ca6cdde77e7272
DatDraggy <github.com@kieran.de>
2019-05-05 20:53:27 +0200

    Create send_single.php

---
+++ backend/mails/send_single.php
@@ -0,0 +1,15 @@
+<?php
+if (php_sapi_name() != "cli") {
+  die('lol nah');
+}
+require_once('../config.php');
+require_once('../funcs.php');
+
+$email = '';
+$nickname = '';
+sendEmail($email, 'Your Summerbo.at Registration has been canceled', "Dear $nickname,
+
+we're sorry to inform you that we had to cancel your Summerbo.at registration because you did not update your Eurofurence registration number in your account.
+
+If you have any questions, please send us a message. Reply to this e-mail or contact us via Telegram at <a href=\"https://t.me/summerboat\">https://t.me/summerboat</a>.
+", true);
