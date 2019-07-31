7ca1c6403d258807bb100d63efcec8284cb64a09
DatDraggy <github.com@kieran.de>
2019-07-31 16:42:48 +0200

    ok looks good

--- backend/scripts/badge_gen.php
+++ backend/scripts/badge_gen.php
@@ -25,5 +25,5 @@ foreach ($texts as $text) {
   ImageTTFText($im, 100, 0, 212, 2605, $white, '../../webfonts/TideSans-700Mondo.ttf', $text[1]);
   ImageTTFText($im, 160, 0, 205, 2925, $white, '../../webfonts/TideSans-700Mondo.ttf', '#' . $text[0]);
 
-  ImagePng($im, "images/{$text[0]}.png", 0, PNG_NO_FILTER);
+  ImagePng($im, "images/{$text[0]}.png");
 }
