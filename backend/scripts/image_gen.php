d502e90b478aa802da536678f99d00ddafdb9f7f
DatDraggy <github.com@kieran.de>
2019-07-31 10:07:36 +0200

    Update image_gen.php

--- backend/scripts/image_gen.php
+++ backend/scripts/image_gen.php
@@ -1,7 +1,4 @@
 <?php
-$white = ImageColorAllocate($im, 255, 255, 255);
-$black = ImageColorAllocate($im, 0, 0, 0);
-
 $texts = array(
   1 => 'Hunter',
   2 => 'Kieran',
@@ -11,6 +8,8 @@ $textsB = array_flip($texts);
 
 foreach ($texts as $text){
   $im = imagecreatefrompng("../../images/logo@2x.png");
+  $white = ImageColorAllocate($im, 255, 255, 255);
+  $black = ImageColorAllocate($im, 0, 0, 0);
 
   ImageTTFText($im, 10, 45, 10, 10, $white, '../../webfonts/38454E_1_0.ttf', $text);
   ImageTTFText($im, 10, 45, 10, 30, $white, '../../webfonts/38454E_1_0.ttf', $textsB[$text]);
