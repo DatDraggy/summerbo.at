9bb38467918ab5bc08c56431e5a73867075c03a0
DatDraggy <github.com@kieran.de>
2019-07-31 11:41:44 +0200

    ya

--- backend/scripts/badge_gen.php
+++ backend/scripts/badge_gen.php
@@ -19,15 +19,14 @@ $texts = array(
     'Longname Goes Here'
   ]
 );
-$textsB = array_flip($texts);
 
 foreach ($texts as $text) {
   $im = imagecreatefrompng("source/crew_front.png");
   $white = ImageColorAllocate($im, 255, 255, 255);
   $black = ImageColorAllocate($im, 0, 0, 0);
 
-  ImageTTFText($im, 10, -45, 165, 2010, $white, '../../webfonts/TideSans-700Mondo.ttf', $text[1]);
-  ImageTTFText($im, 10, -45, 150, 2222, $white, '../../webfonts/TideSans-700Mondo.ttf', '#' . $text[0]);
+  ImageTTFText($im, 80, 0, 165, 2010, $white, '../../webfonts/TideSans-700Mondo.ttf', $text[1]);
+  ImageTTFText($im, 100, 0, 150, 2222, $white, '../../webfonts/TideSans-700Mondo.ttf', '#' . $text[0]);
 
-  ImagePng($im, "images/$text.png");
+  ImagePng($im, "images/{$text[0]}.png");
 }
