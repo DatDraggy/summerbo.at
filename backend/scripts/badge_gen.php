4c10010fa0651d88dcf47cbe88be3997af0cd285
DatDraggy <github.com@kieran.de>
2019-07-31 12:55:02 +0200

    Update badge_gen.php

--- backend/scripts/badge_gen.php
+++ backend/scripts/badge_gen.php
@@ -2,31 +2,28 @@
 $texts = array(
   [
     1,
-    'Hunter'
+    'Hunter',
+    'staff'
   ],
   [
     2,
-    'Kieran'
+    'DatDraggy',
+    'staff'
   ],
   [
     4,
-    'Trust'
-  ]
-);
-$texts = array(
-  [
-    182,
-    'Longname Goes Here'
+    'Trust',
+    'vip'
   ]
 );
 
 foreach ($texts as $text) {
-  $im = imagecreatefrompng("source/crew_front.png");
+  $im = imagecreatefrompng("source/{$text[2]}_front.png");
   $white = ImageColorAllocate($im, 255, 255, 255);
   $black = ImageColorAllocate($im, 0, 0, 0);
 
-  ImageTTFText($im, 82, 0, 172, 2097, $white, '../../webfonts/TideSans-700Mondo.ttf', $text[1]);
-  ImageTTFText($im, 141, 0, 160, 2362, $white, '../../webfonts/TideSans-700Mondo.ttf', '#' . $text[0]);
+  ImageTTFText($im, 100, 0, 212, 2605, $white, '../../webfonts/TideSans-700Mondo.ttf', $text[1]);
+  ImageTTFText($im, 160, 0, 205, 2925, $white, '../../webfonts/TideSans-700Mondo.ttf', '#' . $text[0]);
 
   ImagePng($im, "images/{$text[0]}.png");
 }
