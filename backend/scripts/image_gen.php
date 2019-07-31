07878d60ce92db2e0894b62aafcf5798854e771f
DatDraggy <github.com@kieran.de>
2019-07-31 10:06:29 +0200

    silly testing

---
+++ backend/scripts/image_gen.php
@@ -0,0 +1,19 @@
+<?php
+$white = ImageColorAllocate($im, 255, 255, 255);
+$black = ImageColorAllocate($im, 0, 0, 0);
+
+$texts = array(
+  1 => 'Hunter',
+  2 => 'Kieran',
+  4 => 'Trust'
+);
+$textsB = array_flip($texts);
+
+foreach ($texts as $text){
+  $im = imagecreatefrompng("../../images/logo@2x.png");
+
+  ImageTTFText($im, 10, 45, 10, 10, $white, '../../webfonts/38454E_1_0.ttf', $text);
+  ImageTTFText($im, 10, 45, 10, 30, $white, '../../webfonts/38454E_1_0.ttf', $textsB[$text]);
+
+  ImagePng($im, "images/$text");
+}
