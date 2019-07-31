0c46b676bd492cd6eb430c8baf705d2393797d18
DatDraggy <github.com@kieran.de>
2019-07-31 10:11:10 +0200

    Update image_gen.php

--- backend/scripts/image_gen.php
+++ backend/scripts/image_gen.php
@@ -11,8 +11,8 @@ foreach ($texts as $text){
   $white = ImageColorAllocate($im, 255, 255, 255);
   $black = ImageColorAllocate($im, 0, 0, 0);
 
-  ImageTTFText($im, 10, 45, 10, 10, $white, '../../webfonts/38454E_1_0.ttf', $text);
-  ImageTTFText($im, 10, 45, 10, 30, $white, '../../webfonts/38454E_1_0.ttf', $textsB[$text]);
+  ImageTTFText($im, 10, -45, 10, 50, $white, '../../webfonts/38454E_1_0.ttf', $text);
+  ImageTTFText($im, 10, -45, 10, 70, $white, '../../webfonts/38454E_1_0.ttf', $textsB[$text]);
 
   ImagePng($im, "images/$text.png");
 }
