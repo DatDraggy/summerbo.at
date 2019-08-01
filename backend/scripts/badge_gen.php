b428efa126d4ee13579f4e9c42c510b9adf560ee
DatDraggy <github.com@kieran.de>
2019-08-01 11:22:03 +0200

    testing badge printing

--- backend/scripts/badge_gen.php
+++ backend/scripts/badge_gen.php
@@ -1,5 +1,23 @@
 <?php
-$texts = array(
+$loops = 0;
+//for testing, only first 5 badges
+if (($handle = fopen("badges.csv", "r")) !== FALSE) {
+  while (($data = fgetcsv($handle, 50, ",")) !== FALSE && $loops < 5) {
+    $im = imagecreatefrompng("source/{$data[2]}_front.png");
+    $white = ImageColorAllocate($im, 255, 255, 255);
+    $black = ImageColorAllocate($im, 0, 0, 0);
+
+    ImageTTFText($im, 100, 0, 212, 2605, $white, '../../webfonts/TideSans-700Mondo.ttf', $data[1]);
+    ImageTTFText($im, 160, 0, 205, 2925, $white, '../../webfonts/TideSans-700Mondo.ttf', '#' . $data[0]);
+
+    ImagePng($im, "images/{$data[0]}_{$data[1]}_a.png");
+    copy("source/{$data[2]}_back.png", "images/{$data[0]}_{$data[1]}_b.png");
+    $loops += 1;
+  }
+  fclose($handle);
+}
+
+/*$texts = array(
   [
     1,
     'Hunter',
@@ -17,13 +35,14 @@ $texts = array(
   ]
 );
 
-foreach ($texts as $text) {
-  $im = imagecreatefrompng("source/{$text[2]}_front.png");
+foreach ($texts as $data) {
+  $im = imagecreatefrompng("source/{$data[2]}_front.png");
   $white = ImageColorAllocate($im, 255, 255, 255);
   $black = ImageColorAllocate($im, 0, 0, 0);
 
-  ImageTTFText($im, 100, 0, 212, 2605, $white, '../../webfonts/TideSans-700Mondo.ttf', $text[1]);
-  ImageTTFText($im, 160, 0, 205, 2925, $white, '../../webfonts/TideSans-700Mondo.ttf', '#' . $text[0]);
+  ImageTTFText($im, 100, 0, 212, 2605, $white, '../../webfonts/TideSans-700Mondo.ttf', $data[1]);
+  ImageTTFText($im, 160, 0, 205, 2925, $white, '../../webfonts/TideSans-700Mondo.ttf', '#' . $data[0]);
 
-  ImagePng($im, "images/{$text[0]}.png");
-}
+  ImagePng($im, "images/{$data[0]}_{$data[1]}_a.png");
+  copy("source/{$data[2]}_back.png", "images/{$data[0]}_{$data[1]}_b.png");
+}*/
