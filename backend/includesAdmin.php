7fdfb4fe9d14541ece444588ac6241b008d56b2a
DatDraggy <github.com@kieran.de>
2019-09-05 10:33:45 +0200

    Testing anonymized tracking on admin page

--- backend/includesAdmin.php
+++ backend/includesAdmin.php
@@ -25,6 +25,19 @@ $head = '<meta charset="utf-8">
     <link rel="stylesheet" href="/css/css.css">
     <link rel="stylesheet" href="/css/badger-accordion.css">
     <link rel="stylesheet" href="/css/line-awesome.min.css">
+    <script type="text/javascript">
+      var _paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push([\'trackPageView\']);
+      _paq.push([\'enableLinkTracking\']);
+      (function() {
+        var u="//stats.summerbo.at/";
+        _paq.push([\'setTrackerUrl\', u+\'matomo.php\']);
+        _paq.push([\'setSiteId\', \'1\']);
+        var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];
+        g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src=u+\'matomo.js\'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
 ';
 
 $nav = '<nav id="nav" class="nav inactive">
