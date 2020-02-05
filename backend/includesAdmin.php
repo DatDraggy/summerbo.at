4d947f7ee167ac9f157cb0c7cf10328a2ac146f0
DatDraggy <github.com@kieran.de>
2020-02-05 15:52:49 +0100

    new icons

--- backend/includesAdmin.php
+++ backend/includesAdmin.php
@@ -1,26 +1,28 @@
 <?php
 $head = '<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2020XBbnOXWxGx" color="#3ec1c7">
+    <link rel="shortcut icon" href="/favicon.ico?v=2020XBbnOXWxGx">
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png?v=2020XBbnOXWxGx">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png?v=2020XBbnOXWxGx">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png?v=2020XBbnOXWxGx">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png?v=2020XBbnOXWxGx">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png?v=2020XBbnOXWxGx">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png?v=2020XBbnOXWxGx">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png?v=2020XBbnOXWxGx">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png?v=2020XBbnOXWxGx">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png?v=2020XBbnOXWxGx">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png?v=2020XBbnOXWxGx">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2020XBbnOXWxGx">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png?v=2020XBbnOXWxGx">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2020XBbnOXWxGx">
+    <link rel="manifest" href="/manifest.json?v=2020XBbnOXWxGx">
+    <meta name="msapplication-TileColor" content="#3ec1c7">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png?v=2020XBbnOXWxGx">
+    <meta name="theme-color" content="#3ec1c7">
+
     <meta name="apple-mobile-web-app-title" content="All Paws on Deck 2020 Summerbo.at Party">
     <meta name="application-name" content="All Paws on Deck 2020 Summerbo.at Party">
-    <meta name="msapplication-TileColor" content="#3EC1C7">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-    <meta name="theme-color" content="#3EC1C7">
     <script src="/js/yall-2.2.0.min.js"></script>
     <link rel="stylesheet" href="/css/css.css">
     <link rel="stylesheet" href="/css/badger-accordion.css">
