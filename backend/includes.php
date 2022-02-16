5efe59b8a971769e67dc851256b1d4801f4531bf
DatDraggy <spamme@kieran.de>
2022-02-16 16:53:02 +0100

    remove cf cookie, update matomo script

--- backend/includes.php
+++ backend/includes.php
@@ -73,21 +73,23 @@ $head = '<meta charset="utf-8">
 if ($_SERVER['SERVER_NAME'] !== 'dev.summerbo.at') {
     $head .= '<!-- Matomo -->
     <script type="text/javascript">
-      var _paq = window._paq || [];
+      var _paq = window._paq = window._paq || [];
       /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
       //consent remembered upon click of cookie notice
       //require user consent before processing data
+      _paq.push(["setExcludedQueryParams", ["token"]]);
       _paq.push([\'requireConsent\']);
       _paq.push([\'trackPageView\']);
       _paq.push([\'enableLinkTracking\']);
       (function() {
-        var u="//stats.summerbo.at/";
+        var u="https://stats.summerbo.at/";
         _paq.push([\'setTrackerUrl\', u+\'matomo.php\']);
         _paq.push([\'setSiteId\', \'1\']);
         var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];
-        g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src=u+\'matomo.js\'; s.parentNode.insertBefore(g,s);
+        g.async=true; g.src=u+\'matomo.js\'; s.parentNode.insertBefore(g,s);
       })();
     </script>
+    <noscript><p><img src="https://stats.summerbo.at/matomo.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
     <!-- End Matomo Code -->';
 }
 
