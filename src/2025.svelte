69bc8a52c55814be787af0655d4b2bda424deb58
DatDraggy <spamme@kieran.de>
2026-01-09 18:28:34 +0100

    2025

---
+++ src/2025.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+</script>
+
+<div class="text-content">
+  <h2 class="text-headline">2025, Space Ship</h2>
+
+  <p class="text-large">Elbe, Überseebrücke, Hamburg WIP</p>
+  <p>
+    Even with our new great and grand MS Hamburg, we sold out way too quickly.
+    So we thought, why not have another cruise? So Space Ship: Chill was born where we floated through the Speicherstadt and enjoyed the end of the con a little more calmly.
+  </p>
+  <div class="image-gallery">
+    <img
+      src="/img/2019/photo1-compressed.jpg"
+      alt="Fursuiters in a boat interior lit by colorful lights"
+    />
+    <img
+      src="/img/2019/photo2-compressed.jpg"
+      alt="Fursuiters on the deck of a boat holding a sign which says 2019: Hot Summer Nights"
+    />
+    <img
+      src="/img/2019/photo3-compressed.jpg"
+      alt="Fursuiters socialising on the deck of a boat"
+    />
+    <img
+      src="/img/2019/photo4-compressed.jpg"
+      alt="People dancing infront of a DJ booth, with a mirrored ceiling reflecting them"
+    />
+    <img
+      src="/img/2019/photo5-compressed.jpg"
+      alt="Two fursuiters sit on the upper deck of the boat whilst others wait to board"
+    />
+    <img
+      src="/img/2019/photo6-compressed.jpg"
+      alt="A partygoer poses in silent-disco headphones on their fursuit's head"
+    />
+  </div>
+</div>
+
+<style>
+  .image-gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(288px, 1fr));
+    column-gap: 1rem;
+    row-gap: 1rem;
+    margin-bottom: 2rem;
+  }
+</style>
