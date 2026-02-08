f1c7507183f5459be6c2b355b5e7e277e045cb7e
DatDraggy <spamme@kieran.de>
2026-02-08 22:53:16 +0100

    update faq

--- src/Faq.svelte
+++ src/Faq.svelte
@@ -1,107 +1,113 @@
 <script lang="ts">
-  import Accordion from "./components/Accordion.svelte";
+    import Accordion from "./components/Accordion.svelte";
 </script>
 
 <div class="faq textContent">
-  <h2 class="text-headline text-large">Frequently Asked Questions</h2>
-  <Accordion question="How will the new digital entrance ticket work?">
-    <p>
-      Before boarding, you will need to provide your digital ticket from your email or browser in combination with your legal ID/passport.
-      After boarding, it will not be possible to be re-boarded, so please stay on the boat, otherwise we won't be able to allow you back on.
-    </p>
-  </Accordion>
-  <Accordion question="Will I still be able to get a con badge for Summerbo.at?">
-    <p>
-      A few weeks before the event, we will add a new optional option to the registration system where you can declare if you would like a physical con badge.
-    </p>
-  </Accordion>
-  <Accordion question="How does the double-cruise work?">
-    <p>
-      You're able to select which boat (tunes or talky) you want to be part of and can change your wish at any time, provided there is enough capacity.<br>
-      When both boats slots are filled, you can organize a swap with an attendee whishing to swap to your boat. Send us an email.<br><br>
+    <h2 class="text-headline text-large">Frequently Asked Questions</h2>
+    <Accordion question="How will the new digital entrance ticket work?">
+        <p>
+            Before boarding, you will need to provide your digital ticket from your email or browser in combination with
+            your legal ID/passport.
+            After boarding, it will not be possible to be re-boarded, so please stay on the boat, otherwise we won't be
+            able to allow you back on.
+        </p>
+    </Accordion>
+    <Accordion question="Will I still be able to get a con badge for Summerbo.at?">
+        <p>
+            A few weeks before the event, we will add a new optional option to the registration system where you can
+            declare if you would like a physical con badge.
+        </p>
+    </Accordion>
+    <Accordion question="How does the double-cruise work?">
+        <p>
+            You're able to select which boat (tunes or talky) you want to be part of and can change your wish at any
+            time, provided there is enough capacity.<br>
+            When both boats slots are filled, you can organize a swap with an attendee whishing to swap to your boat.
+            Send us an email.<br><br>
 
-      All other details (location, boarding, departure) are identical. We'll have clear signage to guide you to your designated boat where your entrance ticket will be checked.
-    </p>
-  </Accordion>
-  <Accordion question="Can I freely choose which cruise to board?">
-    <p>
-      No, our checkin will only allow you to board the cruise (tunes or talky) you have selected during registration.
-    </p>
-  </Accordion>
-  <Accordion question="Where are the boats?">
-    <p>
-       You'll find our party or cruise boats at Überseebrücke, 20459 Hamburg. Near train station U Baumwall.
-    </p>
-  </Accordion>
-  <Accordion question="What kind of things can we do at the party?">
-    <p>
-      There will be a <strong>dancefloor</strong>, multiple <strong>bars</strong>,
-      <strong>lounge area</strong> and <strong>a fursuit lounge</strong>. The
-      outside decks provide for a great place to chill, have a drink and mingle
-      with the other guests. Games and events including fursuit limbo,
-      photoshoot, and more will be organized. The best way to stay tuned for
-      announcements and events is via the
-      <a href="https://t.me/summerboatinfo" rel="noopener" target="_blank"
-        >telegram info channel</a
-      >
-      or our
-      <a
-        href="https://www.twitter.com/summerbo_at"
-        rel="noopener"
-        target="_blank">Twitter</a
-      >.
-    </p>
-  </Accordion>
-  <Accordion question="Is this party affiliated with Eurofurence?"
-    ><p>
-      We are part of, officially acknowledged and supported by Eurofurence. We
-      want to offer early arrival attendees a party to kick off their convention
-      experience! Ticket payment will be handled by the official Eurofurence
-      registration system.
-    </p></Accordion
-  >
-  <Accordion question="What is the minimum age for attending?"
-    ><p>
-      You need to be at least 18 years old on the day of the party. You will be
-      required to present a legal photo ID or passport at badge pickup time.
-    </p></Accordion
-  >
-  <Accordion question="Can I order my VIP Upgrade later?"
-    ><p>
-      Yes! Simply login with your account details, tick
-      the box for VIP upgrade in the user area and
-      click submit. You will receive an email as a confirmation.
-    </p></Accordion
-  >
-  <Accordion question="How do I get my badge?"
-    ><p>
-      You will find our Info & Check-in desk at Überseebrücke. Please make sure you bring your ID or Passport with you. The badge
-      is your ticket to the party so please do not lose it!<br />
-      Keep in mind: Badge pickup and Check-in
-      <em><strong>before</strong></em> boarding!
-    </p></Accordion
-  >
-  <Accordion question="Where do the profits of the party go to?"
-    ><p>
-      Our money is managed by the legal entity Eurofurence e.V. which is
-      unable to take any profits for other purposes than the event itself.
-      Leftover money will be rolled over as a reserve for next years party
-      or donated to charity, which we'll be announcing transparently.
-      In previous years, we donated ALL the money we made from VIP upgrades to charity,
-      but due to the move to Hamburg and global crises, we'll actually use it to support
-      the party to be able to not raise prices too much for our regular attendees.
-    </p></Accordion
-  >
+            All other details (location, boarding, departure) are identical. We'll have clear signage to guide you to
+            your designated boat where your entrance ticket will be checked.
+        </p>
+    </Accordion>
+    <Accordion question="Can I freely choose which cruise to board?">
+        <p>
+            No, our checkin will only allow you to board the cruise (tunes or talky) you have selected during
+            registration.
+        </p>
+    </Accordion>
+    <Accordion question="Where are the boats?">
+        <p>
+            You'll find our party or cruise boats at Überseebrücke, 20459 Hamburg. Near train station U Baumwall.
+        </p>
+    </Accordion>
+    <Accordion question="What kind of things can we do at the party?">
+        <p>
+            There will be a <strong>dancefloor</strong>, multiple <strong>bars</strong>,
+            <strong>lounge area</strong> and <strong>a fursuit lounge</strong>. The
+            outside decks provide for a great place to chill, have a drink and mingle
+            with the other guests. Games and events including fursuit limbo,
+            photoshoot, and more will be organized. The best way to stay tuned for
+            announcements and events is via the
+            <a href="https://t.me/summerboatinfo" rel="noopener" target="_blank"
+            >telegram info channel</a
+            >
+            or our
+            <a
+                    href="https://www.twitter.com/summerbo_at"
+                    rel="noopener"
+                    target="_blank">Twitter</a
+            >.
+        </p>
+    </Accordion>
+    <Accordion question="Is this party affiliated with Eurofurence?">
+        <p>
+            We are part of, officially acknowledged and supported by Eurofurence. We
+            want to offer early arrival attendees a party to kick off their convention
+            experience! Ticket payment will be handled by the official Eurofurence
+            registration system.
+        </p>
+    </Accordion>
+    <Accordion question="What is the minimum age for attending?">
+        <p>
+            You need to be at least 18 years old on the day of the party. You will be
+            required to present a legal photo ID or passport at badge pickup time.
+        </p>
+    </Accordion>
+    <Accordion question="Can I order my VIP Upgrade later?">
+        <p>
+            Yes! Simply login with your account details, tick
+            the box for VIP upgrade in the user area and
+            click submit. You will receive an email as a confirmation.
+        </p>
+    </Accordion>
+    <Accordion question="How do I get my badge?">
+        <p>
+            If you selected the optional con badge in your registration, you'll be able to pick it up on the boat during
+            the
+            cruise.
+        </p>
+    </Accordion>
+    <Accordion question="Where do the profits of the party go to?">
+        <p>
+            Our money is managed by the legal entity Eurofurence e.V. which is
+            unable to take any profits for other purposes than the event itself.
+            Leftover money will be rolled over as a reserve for next years party
+            or donated to charity, which we'll be announcing transparently.
+            In previous years, we donated ALL the money we made from VIP upgrades to charity,
+            but due to the move to Hamburg and global crises, we'll actually use it to support
+            the party to be able to not raise prices too much for our regular attendees.
+        </p>
+    </Accordion>
 
-  <Accordion question="How did the party come to be?">
-    <p>
-      In <a href="https://2019.summerbo.at/" target="_blank">2019</a> some friends
-      wanted to do something special for celebrating the 25th edition of Eurofurence
-      and the 5th edition in Berlin, the city that knows how to party! So why not
-      on a boat? Cruising on the beautiful Spree River through Berlin. We wanted
-      a chill atmosphere for partying and suiting up.
-    </p>
-  </Accordion>
+    <Accordion question="How did the party come to be?">
+        <p>
+            In <a href="https://2019.summerbo.at/" target="_blank">2019</a> some friends
+            wanted to do something special for celebrating the 25th edition of Eurofurence
+            and the 5th edition in Berlin, the city that knows how to party! So why not
+            on a boat? Cruising on the beautiful Spree River through Berlin. We wanted
+            a chill atmosphere for partying and suiting up.
+        </p>
+    </Accordion>
 </div>
 
 <style>
